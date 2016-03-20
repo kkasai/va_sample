a911dcc78d3f5fffb3b2fc3fa041d9ff9ee066a7
Keita Kasai <k013c1345@it-neec.jp>
2016-03-20 21:17:53 +0900

    bootstrap.shの作成

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#Apache
+sudo yum -y install httpd
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+
+#firewalld
+sudo systemctl stop firewalld.service
+sudo systemctl disable firewalld.service
+
+#MariaDB
+sudo yum -y remove mariadb*
+sudo rm -rf /var/lib/mysql/
+
+#wget
+sudo yum -y install wget
+
+#vim 
+sudo yum -y install vim
+
+#MySQL
+sudo wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -Uvh mysql-community-release-el7-5.noarch.rpm
+sudo sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/mysql-community.repo
+sudo yum -y --enablerepo=mysql56-community install mysql-community-server 
+sudo systemctl start mysqld.service
+sudo systemctl enable mysqld.service
+
+#PHP
+sudo yum -y install php php-mysql php-mbstring
+sudo systemctl restart httpd.service
+
+# chmod
+sudo chmod -R 777 /var/www/html
